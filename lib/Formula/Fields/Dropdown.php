c92f1f8b8f0b06702c441f7518c95e66c2b1c291
Grant Gingell <ggingell@fsu.edu>
2012-08-07 16:31:06 -0400

    Added: Repopulate the Other field on Dropdown fields.

--- lib/Formula/Fields/Dropdown.php
+++ lib/Formula/Fields/Dropdown.php
@@ -54,18 +54,20 @@ class Dropdown extends Abstracts\MultipleChoiceInput {
 
     //Determine the checked value, if possible
     if ($this->_data) {
-      $val = $this->_data[$this->name];
+      $value = $this->_data[$this->name];
     }
     elseif ($this->defaultValue) {
-      $val = $this->defaultValue;
+      $value = $this->defaultValue;
     }
     else {
-      $val = NULL;
+      $value = NULL;
     }
-
 
     //Flag
     $itemSelected = FALSE;
+
+    //Options values discovered
+    $optionsValuesDiscovered = array();
 
     //Add the options
     $optionsArray = array();
@@ -74,21 +76,22 @@ class Dropdown extends Abstracts\MultipleChoiceInput {
       foreach($this->options as $key => $val) {
 
         $currval  = ($this->useValues) ? $val : $key;
-        $selected = (strcmp($val, $currval) == 0) ? "selected='selected'" : NULL;
+        $selected = (strcmp($value, $currval) == 0) ? "selected='selected'" : NULL;
         $itemSelected = (boolean) $selected;
 
         if (is_array($val)) {
 
-          $currval  = ($this->useValues) ? $v : $k;
-          $selected = (strcmp($val, $currval) == 0) ? "selected='selected'" : NULL;
-
           foreach($val as $k => $v) {
+            $currval  = ($this->useValues) ? $v : $k;
+            $selected = (strcmp($val, $currval) == 0) ? "selected='selected'" : NULL;
             $subOptArray[] = "<option id='{$this->name}_{$k}' value='{$currval}' {$selected}>{$val}</option>";
+            $optionsValuesDiscovered[] = $currval;
           }
 
           $optionsArray[] = "<optgroup id='{$this->name}_{$key}'>" . implode("\n", $subOptArray) . "</optgroup>";
         }
         else {
+          $optionsValuesDiscovered[] = $currval;
           $optionsArray[] = "<option id='{$this->name}_{$key}' value='{$currval}' {$selected}>{$val}</option>";
         }
       }
@@ -99,13 +102,22 @@ class Dropdown extends Abstracts\MultipleChoiceInput {
     //Add 'other' logic if enabled
     $otherHtml = '';
     if ($this->allowOther) {
-      $checked = ($val == '_other') ? "checked='checked'" : NULL;
-      $val = $this->_data[$this->name . '_other_input'];
+
+      //If no data, see if the default value is not in the array of discovered options
+      if (empty($this->_data) && $this->defaultValue) {
+        $checked = ( ! in_array($this->defaultValue, $optionsValuesDiscovered));
+        $value = $this->defaultValue;
+      }
+      else {
+        $checked = ($value == '_other') ? "checked='checked'" : NULL;
+        $value = $this->_data[$this->name . '_other_input'];
+      }
 
       if ($this->otherLabel) {
         $otherHtml .= sprintf("<label class='dropdown_other_label' for='%s_other_input'>%s</label>", $this->id, $this->otherLabel);
       }
-      $otherHtml .= "<input type='text' class='dropdown_other' id='{$this->id}__other_input' name='{$this->name}_other_input' value='{$val}' placeholder='{$this->otherPlaceholder}' />";
+
+      $otherHtml .= "<input type='text' class='dropdown_other' id='{$this->id}__other_input' name='{$this->name}_other_input' value='{$value}' placeholder='{$this->otherPlaceholder}' />";
     }
 
     $labelHtml = ($this->label) ? sprintf(self::$labelHtml, $this->id, $this->label) : NULL;
